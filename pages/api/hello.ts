ecfffaf93b5e06bc38a4a45b508a0ee6f9b15d0c
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-23 16:22:14 +0000

    Create API to answer questions

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  answer: string;
+  answer?: string,
+  status: "success" | "error",
+  error?: string
 };
+
+async function run(model: string, input: { messages: { role: string; content: string }[] }) {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCID}/ai/run/${model}`,
+    {
+      headers: { Authorization: "Bearer " + process.env.CF_TOKEN },
+      method: "POST",
+      body: JSON.stringify(input),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
 
 export default function handler(
   req: NextApiRequest,
@@ -12,6 +26,29 @@ export default function handler(
   //get post data -- question
   const question = req.body.question;
   console.log({ question });
-  
-  res.status(200).json({ answer: "John Doe" });
+  run("@hf/thebloke/mistral-7b-instruct-v0.1-awq", {
+    messages: [
+      {
+        role: "system",
+        content: `You're a 'what if' bot. You can generate 'what if' scenarios based on user input with humor and wit.
+        Describe the drastic effects of it while keeping it fun and humorous.`,
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+  }).then((response) => {
+    try {
+      const result = response.result.response
+
+      res.send({ status: "success", answer: result });
+
+    } catch (e) {
+      console.error(e)
+      console.info(response)
+      res.status(500)
+      .send({ status: "error", error: `I'm very sorry, I was unable to process your request. If you could please try again, I would be very grateful. There may be a rat chewing on my wires. I'm not sure. I hope it's fine.` });
+    }
+  });
 }
