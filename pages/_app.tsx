3cec9846d650b95830142a6afdc8034879356745
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-23 19:08:49 +0000

    Refactor _app.tsx to add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from 'react-hot-toast';
 import { Metadata } from 'next';
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Huh, What if...',
@@ -26,6 +27,19 @@ export const metadata: Metadata = {
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <title>Huh, What if...</title>
+      <meta name="description" content="Imagine what could be possible if you knew the answer to the unknown" />
+
+      {/* Open Graph Meta Tags */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://huh-whatif.vercel.app" />
+      <meta property="og:title" content="Huh, What if..." />
+      <meta property="og:description" content="Imagine what could be possible if you knew the answer to the unknown" />
+      <meta property="og:image" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
+      <meta property="og:image:alt" content="Huh, What if..." />
+    </Head>
+
   <Component {...pageProps} />
   <Toaster/>
   </>;
