8155a1862f3ee7dd2391dbd17554f05ffc3475f4
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-23 19:25:04 +0000

    Add secure URL for og:image in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <meta property="og:title" content="Huh, What if..." />
       <meta property="og:description" content="Imagine what could be possible if you knew the answer to the unknown" />
       <meta property="og:image" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
+      <meta property="og:image:secure_url" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
       <meta property="og:image:alt" content="Huh, What if..." />
       <script defer src="https://umami.hrichik.xyz/script.js" data-website-id="0a20bdb6-769c-432a-9d10-b60eca839914"></script>
     </Head>
