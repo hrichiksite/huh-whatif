09f1bb7daf62863c2ee8979a2e1649db523dd740
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-23 19:12:34 +0000

    Add @vercel/analytics package and update metadata in _app.tsx and package.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,7 @@ import type { AppProps } from "next/app";
 import { Toaster } from 'react-hot-toast';
 import { Metadata } from 'next';
 import Head from 'next/head'
-
-export const metadata: Metadata = {
-  title: 'Huh, What if...',
-  description: 'Imagine what could be possible if you knew the answer to the unknown',
-  metadataBase: new URL('https://cloud-8tz3eq9yk-hack-club-bot.vercel.app'),
-  openGraph: {
-    type: 'website',
-    url: 'https://huh-whatif.vercel.app',
-    title: 'Huh, What if...',
-    description: 'Imagine what could be possible if you knew the answer to the unknown',
-    images: [
-      {
-        url: '/0image.png',
-        width: 1545,
-        height: 1545,
-        alt: 'Huh, What if...',
-      },
-    ],
-  },
-};
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -38,9 +19,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <meta property="og:description" content="Imagine what could be possible if you knew the answer to the unknown" />
       <meta property="og:image" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
       <meta property="og:image:alt" content="Huh, What if..." />
+      <script defer src="https://umami.hrichik.xyz/script.js" data-website-id="0a20bdb6-769c-432a-9d10-b60eca839914"></script>
     </Head>
 
   <Component {...pageProps} />
+  <Analytics/>
   <Toaster/>
   </>;
 }
