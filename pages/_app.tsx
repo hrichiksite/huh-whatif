1bef104f09bd545da8eb3660bfaeb3c2e15a72d9
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-24 05:41:14 +0000

    Fix Script Placement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,8 +20,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <meta property="og:image" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
       <meta property="og:image:secure_url" content="https://cloud-8tz3eq9yk-hack-club-bot.vercel.app/0image.png" />
       <meta property="og:image:alt" content="Huh, What if..." />
-      <Script defer src="https://umami.hrichik.xyz/script.js" data-website-id="0a20bdb6-769c-432a-9d10-b60eca839914"></Script>
     </Head>
+    <Script strategy="afterInteractive" src="https://umami.hrichik.xyz/script.js" data-website-id="0a20bdb6-769c-432a-9d10-b60eca839914"></Script>
 
   <Component {...pageProps} />
   <Analytics/>
